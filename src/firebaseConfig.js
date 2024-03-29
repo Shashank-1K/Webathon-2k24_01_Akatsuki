f9f6a7817395ef0ac631666b3e7a77bcbdcdf95e
Shashank-1K <96000384+Shashank-1K@users.noreply.github.com>
2024-03-29 11:34:32 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,48 +1,16 @@
-// import { initializeApp } from 'firebase/app';
-// import {getAuth,GoogleAuthProvider} from "firebase/auth"
-// import {getDatabase} from "firebase/database"
-// import { getFirestore } from 'firebase/firestore'
-
-// // const firebaseConfig = {
-// //   apiKey: "AIzaSyB5UyU2oHHxHK-rP5asqGaGgeaMy085O0s",
-// //   authDomain: "fir-demo-97d95.firebaseapp.com",
-// //   projectId: "fir-demo-97d95",
-// //   databaseURL : "https://fir-demo-97d95-default-rtdb.asia-southeast1.firebasedatabase.app",
-// //   storageBucket: "fir-demo-97d95.appspot.com",
-// //   messagingSenderId: "1045614018836",
-// //   appId: "1:1045614018836:web:bb3f0bc6cdba8006fcc201",
-// //   measurementId: "G-XZLSVB4W2S"
-// // };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB5UyU2oHHxHK-rP5asqGaGgeaMy085O0s",
-//   authDomain: "fir-demo-97d95.firebaseapp.com",
-//   databaseURL: "https://fir-demo-97d95-default-rtdb.asia-southeast1.firebasedatabase.app",
-//   projectId: "fir-demo-97d95",
-//   storageBucket: "fir-demo-97d95.appspot.com",
-//   messagingSenderId: "1045614018836",
-//   appId: "1:1045614018836:web:bb3f0bc6cdba8006fcc201",
-//   measurementId: "G-XZLSVB4W2S"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth()
-// export const db = getFirestore(app)
-// export const googleProvider = new GoogleAuthProvider()
-// export const realtimedb = getDatabase();
-
 import { initializeApp } from 'firebase/app';
 import {getAuth,GoogleAuthProvider} from "firebase/auth"
 import {getDatabase} from "firebase/database"
 import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "AIzaSyB5UyU2oHHxHK-rP5asqGaGgeaMy085O0s",
-  authDomain: "fir-demo-97d95.firebaseapp.com",
-  projectId: "fir-demo-97d95",
-  databaseURL : "https://fir-demo-97d95-default-rtdb.asia-southeast1.firebasedatabase.app",
-  storageBucket: "fir-demo-97d95.appspot.com",
-  messagingSenderId: "1045614018836",
-  appId: "1:1045614018836:web:bb3f0bc6cdba8006fcc201",
-  measurementId: "G-XZLSVB4W2S"
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  databaseURL : "DATABASE_URL",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER",
+  appId: "API_ID",
+  measurementId: "MEASUREMENT_ID"
 };
 
 const app = initializeApp(firebaseConfig);
